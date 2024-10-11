15d3dc79d70a48b4a71e444379b7687755c66da1
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-10-11 20:27:17 +0300

    .

--- src/index.js
+++ src/index.js
@@ -33,8 +33,33 @@ function createMovie(newMovie) {
 createMovie(newMovie)
 
 
+// function deleteMovieById(id){
+//     const options = {
+//         method: 'DELETE',
+//     }
+//     return fetch(`${BASE_URL}/movies/${id}`, options).then(response => response.json())
+// }
+
+// deleteMovieById(3)
 
 
+const updates = {
+    title: "Start",
+}
+function getPatchId(elementId, updates){
+const options = {
+    method: 'PATCH',
+Headers: {
+    'Content-type ' :'application/json'
+},
+
+body: JSON.stringify(updates)
+}
+return fetch(`${BASE_URL}/movies/${elementId}`, options).then(response => response.json())
+}
+
+
+getPatchId('5', updates)
 
 
 
